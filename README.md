ff694c02dd9284fc6c7dc33fd55b5a57c2031482
ssp3nc3r <ssp3nc3r@users.noreply.github.com>
2020-03-30 00:50:53 -0400

    edits to readme and example code

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # hsluv-rcpp
 Rcpp implementation of HSLuv
+
+## Introduction
+
+HSLuv is a human-friendly way to describe hue, saturation, and luminance --- three attributes of color. The library includes functions to convert this colorspace to computer RGB as separate red, green, and blue values and as a hex code.
+
+## Installation
+
+To install, you will need a working toolchain on your computer for compiling C++ code, and the R package devtools installed. Once those are working, simply run the following code in an R session:
+
+```
+devtools::install_github("ssp3nc3r/hsluv-rcpp", ref = "master")
+```
+
+## Examples
+
+Once the package is installed, the functions can be used to map data to perceptually uniform color, like so:
+
+```
+library(HSLuv); library(ggplot2)
+
+# Create sample data encoded as hue, saturation, luminance
+df <- expand.grid(H = c(20, 290),
+                  S = seq(0, 100, by = 10),
+                  L = seq(0, 100, by = 10))
+
+# Convert HSLuv scaled values to RGB color space as hex code #RRGGBB
+df$colors <- with(df, hsluv_hex(H, S, L) )
+```
+
+Then we can now graph colors encoded with said data:
+
+```
+library(ggplot2)
+
+ggplot(df) +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        axis.text.x.top = element_blank()) + 
+  geom_point(aes(L, S), color = '#eeeeee', fill = df$colors, size = 10, shape = 22) +
+  scale_x_continuous(breaks = seq(0, 100, by = 20),
+                     sec.axis = sec_axis(~., name = 'Hue')) +
+  scale_y_continuous(breaks = seq(0, 100, by = 20)) + facet_wrap(~H) +
+  labs(x = 'Luminance',
+       y = 'Saturation')
+```
+
+returns,
+
+![HSLuv values mapped to RGB](hsl_example.png)
--- hsluv.r
+++ hsluv.r
@@ -1,38 +1,26 @@
 
-# example HSLuv used in ggplot
 
-Rcpp::sourceCpp('hsluv.cpp')
+# Load functions for mapping color https://github.com/ssp3nc3r/hsluv-rcpp
+library(HSLuv)
+
+# Create sample data encoded as hue, saturation, luminance
+df <- expand.grid(H = c(20, 290),
+                  S = seq(0, 100, by = 10),
+                  L = seq(0, 100, by = 10))
+
+# Convert HSLuv scaled values to RGB color space as hex code #RRGGBB
+df$colors <- with(df, hsluv_hex(H, S, L) )
+
+# Plot data encoded as colors
 library(ggplot2)
 
-# represents percentage outcome, maximum of two types
-L <- seq(0, 100, by = 10) 
-
-# percentage point difference between two types: p(type1) - p(type2)
-# sign, then, indicates type
-C <- seq(-100, 100, by = 10) 
-
-# get all combinations
-df <- expand.grid(L = L, C = C) 
-
-# set hue from sign and get abs of diff for chroma / magnitude of diff
-df$H <- ifelse(df$C > 0, 20, 290)
-df$C <- abs(df$C)
-
-# convert values to HSLuv color space
-df_colors <- with(df, hsluv_hex(H, C, L) )
-
-# change strip names
-df$H <- ifelse(df$H != first(df$H), "Hue / Type1 favored", "Hue / Type2 favored")
-
-# plot results
-ggplot(df) + 
+ggplot(df) +
   theme_minimal() +
-  theme(panel.grid = element_blank()) +
-  geom_point(aes(L, C), 
-             fill = df_colors, 
-             size = 5,
-             shape = 22) + 
-  facet_wrap(~H) +
-  labs(x = "Luminance / p(outcome | type favored)",
-       y = "Chroma / abs(p(outcome | type1) - p(outcome | type2)")
-
+  theme(panel.grid = element_blank(),
+        axis.text.x.top = element_blank()) + 
+  geom_point(aes(L, S), color = '#eeeeee', fill = df$colors, size = 10, shape = 22) +
+  scale_x_continuous(breaks = seq(0, 100, by = 20),
+                     sec.axis = sec_axis(~., name = 'Hue')) +
+  scale_y_continuous(breaks = seq(0, 100, by = 20)) + facet_wrap(~H) +
+  labs(x = 'Luminance',
+       y = 'Saturation')
